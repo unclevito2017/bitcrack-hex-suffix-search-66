ee93f608d102524b5a7d888a32d447dbd058d654
Monkey Balls <37158637+unclevito2017@users.noreply.github.com>
2023-09-15 18:22:07 -0400

    Update p66.py

--- p66.py
+++ p66.py
@@ -52,9 +52,8 @@ def generate_and_check(pattern):
         modified_pattern = insert_hex(pattern, hex_string)
         check_match(modified_pattern)
         print("Pattern:", modified_pattern)
-        time.sleep(33)  # Wait for 30 seconds before restarting
-        # input("Press Enter to continue...")
-        # Add additional conditions to check for success or exit the loop if desired
+        time.sleep(33)  # Wait for 33 seconds before restarting adjust for gpu speed
+
 
 # Example usage
 pattern = "xxxxxxx"  # Replace 'xxxxxxx' with the generated hex string
